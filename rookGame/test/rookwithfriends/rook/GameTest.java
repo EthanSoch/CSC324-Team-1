b29ffcaab4aa357db3e2c3698d6bc189d43be6b6
jconrad2016 <jconrad2016@pointloma.edu>
2014-10-29 15:45:26 -0700

    Made test file for Game

---
+++ rookGame/test/rookwithfriends/rook/GameTest.java
@@ -0,0 +1,39 @@
+package rookwithfriends.rook;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetGameBid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetGameBid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTrump() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTrump() {
+		fail("Not yet implemented");
+	}
+
+}
