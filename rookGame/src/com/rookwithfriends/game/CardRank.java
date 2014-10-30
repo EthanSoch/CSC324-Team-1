62021ea0d85e416b77fa180548b798aa381357d4
Ethan Soch <EthanSoch2323@pointloma.edu>
2014-10-29 19:32:46 -0700

    Added getValue();

--- rookGame/src/com/rookwithfriends/game/CardRank.java
+++ rookGame/src/com/rookwithfriends/game/CardRank.java
@@ -27,4 +27,50 @@ public enum CardRank {
 	public double getValue() {
 		return value;
 	}
+	
+	//This method preserves the Enum Type -- Created by Ethan, and Josh -- Refactor when not Cmd Line Based
+	 public static CardRank returnRank(double val)
+		{
+			int num;
+			
+		 	CardRank theValue = null;
+			if (val == 10.5){
+				 num = 15;
+			}
+			else{
+				 num = (int) val;
+			}
+			
+		  switch (num) {
+            case 1:  theValue = one;
+                     break;
+            case 2:  theValue = two;
+                     break;
+            case 3:  theValue = three;
+                     break;
+            case 4:  theValue = four;
+                     break;
+            case 5:  theValue = five;
+                     break;
+            case 6:  theValue = six;
+                     break;
+            case 7:  theValue = seven;
+                     break;
+            case 8:  theValue = eight;
+                     break;
+            case 9:  theValue = nine;
+                     break;
+            case 10: theValue = ten;
+                     break;
+            case 11: theValue = eleven;
+                     break;
+            case 12: theValue = thirteen;
+                     break;
+            case 13: theValue = fourteen;
+            	     break;
+            case 14: theValue = fifteen;
+            		 break;
+            case 15: theValue = rook;
+   		 			 break;
+        }
 }
